52fb68bbd135f2607af3173361d9affb745818f9
Richard Wang <ricky9wang@gmail.com>
2025-02-14 20:40:45 +0800

    feat: import tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import { defineConfig } from 'astro/config';
 
 import cloudflare from '@astrojs/cloudflare';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
